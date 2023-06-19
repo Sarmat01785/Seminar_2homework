b82f28e83f2924a770d43af52c207a64e1e1141f
Sarmat <Spirator@Yandex.ru>
2023-06-19 09:20:43 +0700

    Задача_10 решена двумя способами

--- Task_10/Program.cs
+++ Task_10/Program.cs
@@ -4,3 +4,57 @@
 918 -> 1
 */
 
+void ThreeDigitArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++);
+        
+}
+Console.Write("Введите трехзначное число: ");
+int num = int.Parse(Console.ReadLine());
+
+if (num < 100 || num >= 1000)
+{
+    Console.WriteLine("Введено не трехзначное число");
+    return;
+}
+int temp = num;
+
+int count = 0;
+while (temp != 0)
+{
+    temp = temp / 10; // temp /= 10;
+    count++;
+}
+
+int[] array = new int[count];
+
+for (int i = array.Length - 1; i >= 0; i--)
+{
+    array[i] = num % 10;
+    num /= 10;
+}
+
+Console.Write($"{array[1]}");
+
+/*
+int ThreeDigitNumber(string messag)
+{
+    Console.Write(messag);
+    string res = Console.ReadLine();
+    int result = Convert.ToInt32(res);
+    return result;
+}
+
+int number = ThreeDigitNumber("введите трехзначное число: ");
+
+if (number < 100 || number >= 1000)
+{
+    Console.WriteLine("Введено не трехзначное число");
+    return;
+}
+int rem = number / 10;
+
+int Result = rem % 10;
+
+Console.WriteLine($"{Result}");
+*/
